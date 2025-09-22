ce3a4415372d761f3058c547bbe350b0ed93e291
sorin_cristescu <scr53005@users.noreply.github.com>
2025-09-22 17:48:34 +0200

    trying to fix ENOENT error on vercel build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: undefined,
+  output: 'standalone',
+  images: {
+    domains: ['cdn.innopay.lu'],  // For your custom domain
+  },
 };
 
 export default nextConfig;
